272530e291b214f99fead564dc9ad5054ef4c1be
anisart <anisart92@gmail.com>
2012-10-31 01:29:21 +0400

    many changes

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,9 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-#define F_WIDTH     100
-#define F_HEIGHT    100
-#define PI          3.14159265
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,11 +8,10 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    num = 10;
-    curve = new QwtPlotCurve * [num];
-    mark = new QwtPlotMarker * [num];
+    curve = new QwtPlotCurve * [NUM];
+    mark = new QwtPlotMarker * [NUM];
 
-    for (int j = 0; j < num; ++j)
+    for (int j = 0; j < NUM; ++j)
     {
         curve[j] = new QwtPlotCurve;
         curve[j]->setRenderHint(QwtPlotItem::RenderAntialiased);
@@ -30,8 +26,29 @@ MainWindow::MainWindow(QWidget *parent) :
     grid->setMajPen(QPen(Qt::black,0,Qt::DotLine));
     grid->attach(ui->qwtPlot);
 
+    ///////////////////////////////////////
+    curve2 = new QwtPlotCurve * [NUM];
+    mark2 = new QwtPlotMarker * [NUM];
+
+    for (int j = 0; j < NUM; ++j)
+    {
+        curve2[j] = new QwtPlotCurve;
+        curve2[j]->setRenderHint(QwtPlotItem::RenderAntialiased);
+
+        mark2[j] = new QwtPlotMarker;
+        mark2[j]->setSymbol(new QwtSymbol(QwtSymbol::Ellipse, QColor(Qt::transparent), QColor(Qt::black), QSize(5,5)));
+    }
+
+    ui->qwtPlot2->setAxisScale(QwtPlot::xBottom, -20, 20);
+    ui->qwtPlot2->setAxisScale(QwtPlot::yLeft, -20, 20);
+    grid2 = new QwtPlotGrid;
+    grid2->setMajPen(QPen(Qt::black,0,Qt::DotLine));
+    grid2->attach(ui->qwtPlot2);
+    ///////////////////////////////////////
+
     v = 2;
-    d = 0;//0.5;
+    a = 0.16;
+    r = 5;
     Tmin = 0;
     Tmax = 200;
     count = 100000;
@@ -39,20 +56,41 @@ MainWindow::MainWindow(QWidget *parent) :
     connect(this, SIGNAL(stepChanged(int)), ui->lcdNumber, SLOT(display(int)));
     ui->lcdNumber->setNumDigits(trunc(log10(count)));
 
-    x = new double * [num];
-    y = new double * [num];
+    w = new double[NUM];
+    for (int i = 0; i < NUM; ++i)
+        w[i] = (double)rand()/RAND_MAX * 0.1 + 0.95;
 
-//    vx = new double [num];
-//    vy = new double [num];
+    x = new double * [NUM];
+    y = new double * [NUM];
 
-    for (int j = 0; j < num; ++j)
+    xr = new double * [NUM];
+    yr = new double * [NUM];
+    zr = new double * [NUM];
+
+    f = new double [NUM];
+
+    for (int j = 0; j < NUM; ++j)
     {
         x[j] = new double[count];
         y[j] = new double[count];
 
         x[j][0] = (double)rand()/RAND_MAX * F_WIDTH;
         y[j][0] = (double)rand()/RAND_MAX * F_HEIGHT;
+
+        xr[j] = new double[count];
+        yr[j] = new double[count];
+        zr[j] = new double[count];
+
+        xr[j][0] = -8 + (double)rand()/RAND_MAX * 16;
+        yr[j][0] = -8 + (double)rand()/RAND_MAX * 16;
+        zr[j][0] = -2 + (double)rand()/RAND_MAX * 4;
+
+        f[j] = atan(yr[j][0]/xr[j][0]);
     }
+
+    used.resize(NUM);
+
+    dt = (Tmax - Tmin) / 10000;  // /count;
 
     timer = new QTimer(this);
     connect(timer, SIGNAL(timeout()), this, SLOT(replot()));
@@ -65,38 +103,121 @@ MainWindow::~MainWindow()
 
 void MainWindow::solveStep(int i)
 {
-    for (int j = 0; j < num; ++j)
+    double X1[NUM], X2[NUM], X3[NUM], X4[NUM];
+    double Y1[NUM], Y2[NUM], Y3[NUM], Y4[NUM];
+    double Z1[NUM], Z2[NUM], Z3[NUM], Z4[NUM];
+
+    double X1_[NUM], X2_[NUM], X3_[NUM];
+    double Y1_[NUM], Y2_[NUM], Y3_[NUM];
+    double Z1_[NUM], Z2_[NUM], Z3_[NUM];
+
+    double x_[NUM], y_[NUM], z_[NUM];
+
+    used.clear();
+    used.resize(NUM);
+
+    for (int j = 0; j < NUM; ++j)
     {
-        double theta = (double)rand()/RAND_MAX * 2 * PI - PI;
-        double vx = v * cos(theta);
-        double vy = v * sin(theta);
+        x_[j] = xr[j][i-1];
+        y_[j] = yr[j][i-1];
+        z_[j] = zr[j][i-1];
+
+        d[j].clear();
+
+        for (int k = 0; k < NUM; ++k)
+        {
+            if ((x[k][i-1] - x[j][i-1]) * (x[k][i-1] - x[j][i-1])
+                    + (y[k][i-1] - y[j][i-1]) * (y[k][i-1] - y[j][i-1]) < r * r)
+                d[j].append(k);
+        }
+    }
+
+    for (int j = 0; j < NUM; ++j)
+    {
+        X1[j] = dx(x_, y_, z_, j, i) * dt;
+        Y1[j] = dy(x_, y_, z_, j, i) * dt;
+        Z1[j] = dz(x_, y_, z_, j, i) * dt;
+
+        X1_[j] = xr[j][i-1] + X1[j] / 2;
+        Y1_[j] = yr[j][i-1] + Y1[j] / 2;
+        Z1_[j] = zr[j][i-1] + Z1[j] / 2;
+    }
+
+    for (int j = 0; j < NUM; ++j)
+    {
+        X2[j] =dx(X1_, Y1_, Z1_, j, i) * dt;
+        Y2[j] =dy(X1_, Y1_, Z1_, j, i) * dt;
+        Z2[j] =dz(X1_, Y1_, Z1_, j, i) * dt;
+
+        X2_[j] = xr[j][i-1] + X2[j] / 2;
+        Y2_[j] = yr[j][i-1] + Y2[j] / 2;
+        Z2_[j] = zr[j][i-1] + Z2[j] / 2;
+    }
+
+    for (int j = 0; j < NUM; ++j)
+    {
+        X3[j] = dx(X2_, Y2_, Z2_, j, i) * dt;
+        Y3[j] = dy(X2_, Y2_, Z2_, j, i) * dt;
+        Z3[j] = dz(X2_, Y2_, Z2_, j, i) * dt;
+
+        X3_[j] = xr[j][i-1] + X3[j];
+        Y3_[j] = yr[j][i-1] + Y3[j];
+        Z3_[j] = zr[j][i-1] + Z3[j];
+    }
+
+    for (int j = 0; j < NUM; ++j)
+    {
+        X4[j] = dx(X3_, Y3_, Z3_, j, i) * dt;
+        Y4[j] = dy(X3_, Y3_, Z3_, j, i) * dt;
+        Z4[j] = dz(X3_, Y3_, Z3_, j, i) * dt;
+
+        xr[j][i] = xr[j][i-1] + (X1[j] + 2 * X2[j] + 2 * X3[j] + X4[j]) / 6;
+        yr[j][i] = yr[j][i-1] + (Y1[j] + 2 * Y2[j] + 2 * Y3[j] + Y4[j]) / 6;
+        zr[j][i] = zr[j][i-1] + (Z1[j] + 2 * Z2[j] + 2 * Z3[j] + Z4[j]) / 6;
+
+        f[j] = atan(yr[j][i]/xr[j][i]);
+
+        if (!used.testBit(j))
+        {
+            comp.clear();
+            dfs(j);
+            double theta1 = (double)rand()/RAND_MAX * 2 * PI - PI;
+//            qDebug()<<"===";
+            for (int k = 0; k < comp.size(); ++k)
+            {
+//                qDebug()<<comp.at(k)<<f[comp.at(k)];
+                for (int l = k+1; l < comp.size(); ++l)
+                {
+                    if (fabs(f[comp.at(k)] - f[comp.at(l)]) < 0.01)
+                        qDebug()<<comp.at(k)<<"="<<comp.at(l);
+                }
+            }
+        }
+
+//        qDebug()<<"["<<j<<"] "<<f[j];
+    }
+
+    double *theta = new double [NUM];
+    for (int j = 0; j < NUM; ++j)
+    {
+        theta[j] = (double)rand()/RAND_MAX * 2 * PI - PI;
+        double vx = v * cos(theta[j]);
+        double vy = v * sin(theta[j]);
 
         x[j][i] = x[j][i-1] + vx;
         y[j][i] = y[j][i-1] + vy;
 
         if (x[j][i] < 0)
-        {
             x[j][i] = -x[j][i];
-            vx = -vx;
-        }
 
         if (x[j][i] > F_WIDTH)
-        {
             x[j][i] = 2 * F_WIDTH - x[j][i];
-            vx = -vx;
-        }
 
         if (y[j][i] < 0)
-        {
             y[j][i] = -y[j][i];
-            vy = -vy;
-        }
 
         if (y[j][i] > F_HEIGHT)
-        {
             y[j][i] = 2 * F_HEIGHT - y[j][i];
-            vy = -vy;
-        }
     }
 }
 
@@ -106,22 +227,42 @@ void MainWindow::replot()
     t++;
     solveStep(t);
 
-    for (int j = 0; j < num; ++j)
+    for (int j = 0; j < NUM; ++j)
     {
-        QVector <double> xr, yr;
+        QVector <double> xtrail, ytrail;
         int ind = std::max(0, t - 3000 / (int)Tmax);
         for (int l = ind; l < t; l++)
         {
-            xr.append(x[j][l]);
-            yr.append(y[j][l]);
+            xtrail.append(x[j][l]);
+            ytrail.append(y[j][l]);
         }
 
-        curve[j]->setSamples(xr, yr);
+        curve[j]->setSamples(xtrail, ytrail);
         curve[j]->attach(ui->qwtPlot);
         mark[j]->setValue(x[j][t-1], y[j][t-1]);
         mark[j]->attach(ui->qwtPlot);
     }
     ui->qwtPlot->replot();
+
+    //////////////////////////////////
+    for (int j = 0; j < NUM; ++j)
+    {
+        QVector <double> xtrail, ytrail;
+        int ind = std::max(0, t - 50000 / (int)Tmax);
+        for (int l = ind; l < t; l++)
+        {
+            xtrail.append(xr[j][l]);
+            ytrail.append(yr[j][l]);
+        }
+
+        curve2[j]->setSamples(xtrail, ytrail);
+        curve2[j]->attach(ui->qwtPlot2);
+        mark2[j]->setValue(xr[j][t-1], yr[j][t-1]);
+        mark2[j]->attach(ui->qwtPlot2);
+    }
+    ui->qwtPlot2->replot();
+
+    //////////////////////////////////
 
     if (t > count - 2)
     {
@@ -140,7 +281,27 @@ void MainWindow::on_pauseBtn_clicked()
     }
     else
     {
-        timer->start(50);
+        timer->start(10);
         ui->pauseBtn->setText("Pause");
+    }
+}
+
+void MainWindow::on_syncBox_clicked()
+{
+    if (ui->syncBox->isChecked());//
+//        d = 0.5;
+    else;//
+//        d = 0;
+}
+
+void MainWindow::dfs(int k)
+{
+    used.setBit(k,true);
+    comp.append(k);
+    for (int i = 0; i < d[k].size(); ++i)
+    {
+        int to = d[k].at(i);
+        if (!used.testBit(to))
+            dfs(to);
     }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -8,6 +8,13 @@
 #include <qwt_plot_marker.h>
 #include <qwt_symbol.h>
 #include <qwt_plot_grid.h>
+#include <QBitArray>
+
+#define F_WIDTH     100
+#define F_HEIGHT    100
+#define PI          3.14159265
+#define NUM         10
+#define D_VAL       2
 
 namespace Ui {
 class MainWindow;
@@ -23,21 +30,48 @@ public:
     
 private:
     Ui::MainWindow *ui;
-    QwtPlotCurve **curve;
-    QwtPlotMarker **mark;
+    QwtPlotCurve **curve, **curve2;
+    QwtPlotMarker **mark, **mark2;
     QTimer *timer;
-    QwtPlotGrid *grid;
+    QwtPlotGrid *grid, *grid2;
+//    double d[NUM][NUM];
 
-    double **x, **y, **z;
-    double *w;
-    double d, a, mu, r, v;
+    double **x, **y, **xr, **yr, **zr;
+    double *w, *f;
+    double a, r, v;
     double Tmin, Tmax, dt;
-    int count, num;
+    int count;
     int t;
 
-//    double *vx, *vy;
+    QBitArray used;
+    QVector <int> comp;
+    QVector <int> d[NUM];
 
     void solveStep(int i);
+    void dfs(int k);
+
+    double dx(double *X, double *Y, double *Z, int k, int l)
+    {
+            return -w[k] * Y[k] - Z[k];
+    }
+
+
+    double dy(double *X, double *Y, double *Z, int k, int l)
+    {
+            double sync = 0;
+            int i;
+            foreach (i, d[k])
+                sync += D_VAL * (Y[i] - Y[k]);
+
+            return w[k] * X[k] + a * Y[k] + sync;
+    }
+
+    double dz(double *X, double *Y, double *Z, int k, int l)
+    {
+            return 0.1 + Z[k] * (X[k] - 8.5);
+    }
+
+    friend QDebug operator<< (QDebug dbg, const QBitArray& array);
 
 public slots:
     void replot();
@@ -47,6 +81,16 @@ signals:
 
 private slots:
     void on_pauseBtn_clicked();
+    void on_syncBox_clicked();
 };
+
+inline QDebug operator<< (QDebug dbg, const QBitArray& array)
+{
+    QString text;
+    for (int i = 0; i < array.size(); ++i)
+        text += array.testBit(i) ? "1": "0";
+    dbg << text;
+    return dbg;
+}
 
 #endif // MAINWINDOW_H
